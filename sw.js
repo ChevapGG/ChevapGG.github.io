19b17f9d5d2d79fb6b7492f1d24e2e19a0a12cb0
Dominic <berged@live.de>
2019-06-10 23:46:49 +0200

    final changes

--- sw.js
+++ sw.js
@@ -1,72 +1,32 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/Unit_Converter.html',
-  '/Unit_Converter.css',
-  '/Unit_ConverterV2.js',
-  '/TA.png',
-  '/TA-192x192.png'
-];
+const cacheName = "v4";
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        '/Unit_Converter.html',
+        '/Unit_Converter.css',
+        '/Unit_ConverterV2.js',
+        '/TA.png',
+        '/TA-192x192.png'
+      ]))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
+      self.skipWaiting();
+    }
+  });
+
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-
+      .then(function (response) {
         if (response) {
           return response;
         }
-
-        return fetch(event.request).then(
-          function(response) {
-     
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+        return fetch(event.request);
       })
-    );
+  );
 });
-self.addEventListener('activate', function(event) {
-
-    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-  
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
-  self.addEventListener('install', function(event) {
-    // The promise that skipWaiting() returns can be safely ignored.
-    self.skipWaiting();
-  
-    // Perform any other actions required for your
-    // service worker to install, potentially inside
-    // of event.waitUntil();
-  });
